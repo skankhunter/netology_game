8e8e4d11b1a7e3ed0fbc4e3437598bc36ab03efc
skankhunter <intelmax3@gmail.com>
2018-04-24 16:53:42 +0300

    Добавил Vector

--- game.js
+++ game.js
@@ -1,1 +1,36 @@
 'use strict';
+class Vector {
+    constructor (x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+    plus (vector) {
+        try {
+            vector;
+        } catch (e) {
+            if (e instanceof TypeError) {
+                console.log(e.name + ': ' + e.message);
+                console.log(`Можно прибавлять к вектору только вектор типа Vector`);
+            }
+        }
+        let newVector = {
+            x : this.x + vector.x,
+            y : this.y + vector.y
+        };
+        return newVector
+    }
+
+    times (num) {
+        let newVector = {
+            x : this.x * num,
+            y : this.y * num
+        };
+        return newVector
+    }
+}
+
+class  Actor {
+    constructor (position, size, speed) {
+
+    }
+}
