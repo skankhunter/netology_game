67289edec35190cda870178c996ef1a2971f3873
skankhunter <intelmax3@gmail.com>
2018-05-04 11:50:43 +0300

    Все сделал

--- game.js
+++ game.js
@@ -301,11 +301,52 @@ class FireRain extends Fireball{
     constructor(pos) {
         super();
         this.posRain = pos;
-        this.size = new Vector(1,1);
-        this.speed = new Vector(0,3);
+        this.size = new Vector(1, 1);
+        this.speed = new Vector(0, 3);
     }
 
     handleObstacle() {
        this.pos = this.posRain
+    }
+}
+
+class Coin extends Actor{
+    constructor(pos = new Vector(0, 0)) {
+        super(pos.plus(new Vector(0.2, 0.1)), new Vector(0.6, 0.6));
+        this.size = new Vector(0.6, 0.6);
+        this.startPos = this.pos;
+        this.spring = Math.random() * (Math.PI * 2);
+        this.springSpeed = 8;
+        this.springDist = 0.07;
+    }
+    get type () {
+        return 'coin'
+    }
+
+    updateSpring (time = 1) {
+        this.spring += this.springSpeed * time;
+    }
+
+    getSpringVector () {
+        return new Vector (0, Math.sin(this.spring) * this.springDist);
+    }
+
+    getNextPosition (time = 1) {
+        this.updateSpring(time);
+        return this.startPos.plus(this.getSpringVector());
+    }
+
+    act (time = 1) {
+        this.pos = this.getNextPosition(time);
+    }
+}
+
+class Player extends Actor{
+    constructor(pos = new Vector(0,0)) {
+        super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
+        this.speed = new Vector(0, 0);
+    }
+    get type() {
+        return 'player'
     }
 }
